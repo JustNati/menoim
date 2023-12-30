0b961246a9aecb2e9254c04618df5dc05ab7f9e3
menoim.kanaf4@gmail.com <menoim.kanaf4@gmail.com>
2023-12-30 18:02:59 +0000

    Created utils; Edited Main, NewPerformanceTestForm, NewPerformanceTestForm.RowTemplate1, PerformanceTest and 2 more

--- client_code/NewPerformanceTestForm/__init__.py
+++ client_code/NewPerformanceTestForm/__init__.py
@@ -1,6 +1,10 @@
 from ._anvil_designer import NewPerformanceTestFormTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 from datetime import datetime
+from ..utils import *
 # TODO: tail list
 
 class NewPerformanceTestForm(NewPerformanceTestFormTemplate):
@@ -9,8 +13,53 @@ class NewPerformanceTestForm(NewPerformanceTestFormTemplate):
     self.init_components(**properties)
     today = datetime.now()
     self.test_date.date = f"{today.day}/{today.month}/{today.year}"
+    a = [x['name'] for x in app_tables.powertests.list_columns()]
+    engines = sorted([r['engine_num'] for r in app_tables.engines.search()])
+    self.engine_num_dd.items = engines
+ 
+  def update_test(self):
+    last_ref = app_tables.powertests.search(tables.order_by('reference', ascending=False))[0]['reference']
+    default_values = ['',None]
+    #['','בחרו מנוע','בחרו מזין נתונים', 'בחרו מאשר נתונים', 'בחרו שמישות']
+    new_record = {
+      'reference' : last_ref+1,
+      'engine_num' : self.engine_num_dd.selected_value,
+      'tail' : self.tail_num_info_lbl.text,
+      'side' : self.side_info_lbl.text,
+      'assmble_data' : self.assemble_date.text,
+      'assmble_eng_hours' : self.time_on_asseble.text,
+      'test_date' : self.test_date.date,
+      'cur_engine_hours' : self.engine_time.text,
+      'max_env_temp_OAT' : self.max_env_tmp_txt.text,
+      'barometric_pressure' : self.barometric_pressure_txt.text,
+      'engine_torque' : self.engine_torque_txt.text,
+      'test_n1_rpm' : self.n1_rpm_txt.text,
+      'test_itt' : self.out_itt_ff_temp_txt.text,
+      'test_wf' : self.out_wf_ff_txt.text,
+      'required_wf' : self.required_wf_ff_txt.text,
+      'test_notes' : self.notes_area.text,
+      'submitter' : self.submitter_name_dd.selected_value,
+      'approver' : self.approver_name_dd.selected_value,
+      'test_result' : self.usability_dd.selected_value
+    }
 
+    is_valid_record = validate_empty_values(new_record, default_values)
+    if not is_valid_record or self.engine_num_dd.selected_value is None:
+      Notification("A message",
+             title="A message title",
+             style="danger").show()
+  
   def load_engine_data_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.engine_info_panel.visible = True
-    self.test_form_panel.visible = True
+    print(self.engine_num_dd.selected_value)
+    if self.engine_num_dd.selected_value is None:
+      Notification("A message",
+             title="A message title",
+             style="danger").show()
+    else:
+      self.engine_info_panel.visible = True
+      self.test_form_panel.visible = True
+
+  def update_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.update_test()
---
+++ client_code/utils.py
@@ -0,0 +1,57 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from . import Module1
+#
+#    Module1.say_hello()
+#
+def find_nearest_value(arr: list, target: float):
+    closest_value = arr[0]  # Initialize with the first value in the array
+    min_difference = abs(target - closest_value)  # Initialize the minimum difference
+
+    for value in arr:
+        difference = abs(target - value)
+        if difference < min_difference:
+            min_difference = difference
+            closest_value = value
+
+    return closest_value
+  
+def calc_ng(x: float) -> tuple:
+  m = 1.46 if x<44 else 0.33
+  c = 812 if x<44 else 857.5
+  return m, c
+
+def calc_itt(x: float) -> tuple:
+  m = 3 if x<43.5 else 2
+  c = 655 if x<43.5 else 696
+  return m, c
+
+def validate_empty_values(dictionary, values_list):
+    # Check if any values in the dictionary are empty or match any value in the list
+    for val in dictionary.values():
+        if val == "" or val in values_list:
+            return False
+    return True
+
+def calc_barometric_pressure(x: float, barometric_pressure_val: float,
+                            barometric_vaues = [24,26,28,29.97,31]) -> tuple:
+  m = 0.17 if x<44 else -3
+  c = 0
+  barometric_val = find_nearest_value(barometric_vaues, barometric_pressure_val)
+                              
+  if barometric_val == 24:
+    c = 427.5 if x<44 else 565
+  elif barometric_val == 26:
+    c = 462.5 if x<44 else 575
+  elif barometric_val == 28:
+    c = 502.5 if x<44 else 635
+  elif barometric_val == 29.97:
+    c = 532.5 if x<44 else 670
+  elif barometric_val == 31:
+    c = 547.5 if x<44 else 690
+  return m, c
