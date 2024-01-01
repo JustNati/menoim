c2b5bd26ada76f6f2a61e5606d67188c2ee9004c
menoim.kanaf4@gmail.com <menoim.kanaf4@gmail.com>
2024-01-01 19:54:03 +0000

    Edited TorqueCalc

--- client_code/TorqueCalc/__init__.py
+++ client_code/TorqueCalc/__init__.py
@@ -10,6 +10,10 @@ class TorqueCalc(TorqueCalcTemplate):
     self.init_components(**properties)
 
   def calc_torque(self, **event_args):
-    self.final_result.text = (self.desired_torque * self.torque_len) / (self.torque_len + self.extention_len)
-    self.final_result.visible = true
+    if (self.extention_len.text is None or self.desired_torque.text == None or self.torque_len.text == None): #בודק שהמשתמש הכניס את כל הנתונים
+      self.final_result.visible = False
+      return False
+    else: # אם כל הנתונים הוכנסו
+      self.final_result.text = (self.desired_torque.text * self.torque_len.text) / (self.torque_len.text + self.extention_len.text) # נוסחה לחישוב ערך טורק
+      self.final_result.visible = True 
     # Any code you write here will run before the form opens.
