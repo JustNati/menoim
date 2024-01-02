75001d1f37c4946d283e5f479fbf1bc59f87f937
menoim.kanaf4@gmail.com <menoim.kanaf4@gmail.com>
2024-01-02 15:10:08 +0000

    Created ChecksHistoryByEngine.RowTemplate3 and TestsHistory.RowTemplate4; Edited theme.css and TestsHistory

--- client_code/ChecksHistoryByEngine/RowTemplate3/__init__.py
+++ client_code/ChecksHistoryByEngine/RowTemplate3/__init__.py
@@ -1,17 +1,13 @@
-from ._anvil_designer import TestsHistoryTemplate
+from ._anvil_designer import RowTemplate3Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class TestsHistory(TestsHistoryTemplate):
+class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.repeating_panel_tests.items = anvil.server.call('get_powertest')
-    self.data_grid_1.role = 'wide'
-    self.data_grid_1.columns[0]['width'] = 500
-    #self.data_grid_1.columns = self.data_grid_1.columns
