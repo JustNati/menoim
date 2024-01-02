daf33dbddea5be63ae22e8b499ee5a9d0d8ef6f2
menoim.kanaf4@gmail.com <menoim.kanaf4@gmail.com>
2024-01-02 14:06:47 +0000

    Created Form1.RowTemplate1, Form1 and ChecksHistoryByEnginetemp; Edited settings; Deleted ChecksHistoryByEngine

--- client_code/ChecksHistoryByEnginetemp/__init__.py
+++ client_code/ChecksHistoryByEnginetemp/__init__.py
@@ -1,16 +1,17 @@
-from ._anvil_designer import ChecksHistoryByEngineTemplate
+from ._anvil_designer import ChecksHistoryByEnginetempTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ChecksHistoryByEngine(ChecksHistoryByEngineTemplate):
+class ChecksHistoryByEnginetemp(ChecksHistoryByEnginetempTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     engines = sorted([r['engine_num'] for r in app_tables.engines.search()])
     self.engine_num_dd.items = engines
+
     # Any code you write here will run before the form opens.
 
   def load_engine_data(self):
@@ -23,8 +24,6 @@ class ChecksHistoryByEngine(ChecksHistoryByEngineTemplate):
     self.main_data_grid.visible = True
     self.main_data_grid.items = anvil.server.call('get_powertests')
 
-    
-  
   def engine_num_dd_change(self, **event_args):
     """This method is called when an item is selected"""
     self.load_engine_data()
